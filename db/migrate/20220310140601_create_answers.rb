c49b702ff03e27486f85a386f00a8055d6560ab4
Th <thauanandre09@gmail.com>
2022-03-10 15:57:24 -0300

    refatorando para boas praticas do MVC

--- db/migrate/20220310140601_create_answers.rb
+++ db/migrate/20220310140601_create_answers.rb
@@ -1,9 +1,9 @@
 class CreateAnswers < ActiveRecord::Migration[5.2]
   def change
     create_table :answers do |t|
-      t.text :description
       t.references :question, foreign_key: true
-      t.boolean :correct
+      t.text :description, null: false
+      t.boolean :correct , default: false
 
       t.timestamps
     end
